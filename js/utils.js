9ef2bdd1894466ffc43a27c0e26aec21a4ee0ad7
Ilya Levin <ilya432@gmail.com>
2019-12-11 11:48:28 +0200

    initial commit

---
+++ js/utils.js
@@ -0,0 +1,41 @@
+'use strict'
+
+function getCelectedValue(strClass) {
+    var filterDropDown = document.querySelector(strClass);
+    var value = filterDropDown.options[filterDropDown.selectedIndex].text;
+    return value.toLowerCase();
+}
+
+function fitToContainer(canvas) {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+}
+
+function point(x, y, hex) {
+    this.x = x;
+    this.y = y;
+    this.hex = hex;
+}
+function recordPoints(currentX, currentY, ctx) {
+    var p = ctx.getImageData(currentX, currentY, 1, 1).data;
+    var hex = rgbToHex(p[0], p[1], p[2]);
+    gPoints.push(new point(currentX, currentY, hex));
+    console.log(gPoints);
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
