529649703d73ee3c38f879477a01a27c487c6939
Ilya Levin <ilya432@gmail.com>
2019-12-12 13:26:24 +0200

    added local storage and more buttons

--- js/appService.js
+++ js/appService.js
@@ -5,50 +5,23 @@ var gCanvas, gCtx, gImages = [], gNextImgID = -1;
 //Images
 var gCurrImageName;
 //Colors
-var gBorderColor, gFillColor, gLineSize = 1;
+var gBorderColor, gFillColor;
 //Pos
 var gCurrX = 0, gPrevX = 0, gCurrY = 0, gPrevY = 0;
 var gPoints = [], gScrollTop = 0;
 //keys
 var isDown = false;
+//Text
+var gLineSize = 1, gFontSize = 40;
 
-var gFillColorWell, gBorderColorWell;
 
 
-
-function createImage(name, keywords) {
-    var image = {
-        id: gNextCanvasID++,
-        url: './img/savedMemes/' + name + '.jpg',
-        keywords: []
-    }
-    return image;
-}
-function createImages() {
-    gImages.push(createImage('003'))
-    gImages.push(createImage('004'))
-    gImages.push(createImage('005'))
-    gImages.push(createImage('5'))
-    gImages.push(createImage('006'))
-    gImages.push(createImage('8'))
-    gImages.push(createImage('9'))
-    gImages.push(createImage('12'))
-    gImages.push(createImage('Ancient-Aliens'))
-    gImages.push(createImage('img5'))
-    gImages.push(createImage('img11'))
-    gImages.push(createImage('img12'))
-    gImages.push(createImage('leo'))
-    gImages.push(createImage('meme1'))
-    save();
-}
 
 
 init();
 
 function init() {
-    // debugger;
     gCanvas = document.querySelector('.canvas');
-    // fitToContainer(gCanvas);
     gCtx = gCanvas.getContext("2d");
     gCanvas.addEventListener("mousemove", function (e) {
         move('move', e)
@@ -75,6 +48,9 @@ function init() {
     gCanvas.addEventListener("touchmove", function (e) {
         move('touchmove', e)
     }, false);
+
+    //Images
+    loadCanvasImages();
 }
 
 function move(res, e) {
@@ -135,21 +111,42 @@ function up(res, e) {
     }
 }
 
+function createImage(name, keywords) {
+    var image = {
+        name: name,
+        id: gNextImgID++,
+        DataUTI: gCanvas.toDataURL(),
+        keywords: keywords
+    }
+    saveToStorage('gNextImageID', gNextImgID);
+    return image;
+}
+function createImages() {
+    gImages.push(createImage('003', ['funny']));
+    gImages.push(createImage('004', ['fail', 'lol']));
+    gImages.push(createImage('005', ['funny', 'awesome']));
+    gImages.push(createImage('5', ['lol']));
+    gImages.push(createImage('006', ['funny']));
+    gImages.push(createImage('8', ['lol']));
+    gImages.push(createImage('9', ['fail']));
+    saveCanvasImages();
+    console.log(gImages);
+}
 
 
 window.addEventListener("load", startup, false);
 function startup() {
-    gFillColorWell = document.querySelector(".color-picker-fill");
+    var fillColorPicker = document.querySelector(".color-picker-fill");
     gFillColor = getRandomColor();
-    gFillColorWell.value = gFillColor;
-    gFillColorWell.addEventListener("input", updateFillColor, false);
-    gFillColorWell.select();
+    fillColorPicker.value = gFillColor;
+    fillColorPicker.addEventListener("input", updateFillColor, false);
+    fillColorPicker.select();
 
-    gBorderColorWell = document.querySelector(".color-picker-border");
+    var borderColorPicker = document.querySelector(".color-picker-border");
     gBorderColor = getRandomColor();
-    gBorderColorWell.value = gBorderColor;
-    gBorderColorWell.addEventListener("input", updateBorderColor, false);
-    gBorderColorWell.select();
+    borderColorPicker.value = gBorderColor;
+    borderColorPicker.addEventListener("input", updateBorderColor, false);
+    borderColorPicker.select();
 }
 function updateFillColor(event) {
     gFillColor = event.target.value;
@@ -160,53 +157,49 @@ function updateBorderColor(event) {
     isRandomColors = false;
 }
 
-
-// window.onscroll = function () {
-//     gScrollTop = document.body.scrollTop;
-// };
-
 function clearCanvas() {
     gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
 }
-function saveCanvasImage() {
-    debugger;
-    var imgAsDataURL = gCanvas.toDataURL("image/jpg");
-    // saveToStorage(gImageName, imgAsDataURL);
-
-    try {
-        // localStorage.setItem("elephant", imgAsDataURL);
-        saveToStorage(gCurrImageName, imgAsDataURL);
-    }
-    catch (e) {
-        console.log("Storage failed: " + e);
-    }
+function saveCanvasImages() {
+    saveToStorage('Images', gImages);
 }
-function loadCanvases() {
-    gImages = loadFromStorage('gImages', []);
+function loadCanvasImages() {
+    gImages = loadFromStorage('Images', []);
     gNextImgID = loadFromStorage('gNextImgID', 1);
 
     if (gImages.length === 0) {
         createImages();
     }
-    console.log(gImages);
+    else
+        console.log(gImages);
 }
 
-function increaseLineSize() {
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
-    gLineSize = value;
+function increaseFontSize() {
+    gFontSize++;
+    var textbox = document.querySelector('.input-line-text');
+    drawText(textbox);
+    console.log('font' + gFontSize);
 }
-function decreaseLineSize() {
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value < 1 ? value = 1 : '';
-    value--;
-    document.getElementById('number').value = value;
-    gLineSize = value;
+function decreaseFontSize() {
+    if (gFontSize != 0)
+        gFontSize--;
+    var textbox = document.querySelector('.input-line-text');
+    drawText(textbox);
+    console.log('font' + gFontSize);
 }
+function drawText(elTextBox) {
 
+    var text = elTextBox.value;
+    gCtx.font = `${gFontSize}px Arial`;
+
+    if (text.length > 0) {
+        clearCanvas();
+        gCtx.fillText(text, 100, 100)
+        measureText(gCtx, text)
+    } else {
+        clearCanvas();
+    }
+}
 
 
 
@@ -229,28 +222,15 @@ function loadImageFromInput(ev, onImageReady) {
     }
     reader.readAsDataURL(ev.target.files[0]);
 }
-
-
-
-function drawText(elTextBox) {
-    var text = elTextBox.value;
-    gCtx.font = "30px Arial";
-    if (text.length > 0) {
-        clearCanvas();
-        gCtx.fillText(text, 100, 100)
-        measureText(gCtx, text)
-    } else {
-        clearCanvas();
-    }
-}
-
-
 function displayImage() {
     const dataURI = gCanvas.toDataURL();
     console.log(dataURI);
 }
-
 function downloadImg(elLink) {
     var imgContent = gCanvas.toDataURL('image/jpeg');
     elLink.href = imgContent
 }
+
+
+
+
